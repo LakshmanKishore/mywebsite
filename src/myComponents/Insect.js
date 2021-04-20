7320982248e1897b96a577b12ea79429fef07477
LakshmanKishore <lakshmankishore17.com>
2021-04-20 19:26:26 +0530

    insectphotography h1 to h2

--- src/myComponents/Insect.js
+++ src/myComponents/Insect.js
@@ -11,7 +11,7 @@ export const Insect = () => {
     }
     return (
         <div className="container">
-            <h1 className="text-center pt-3">Insectphotography</h1>
+            <h2 className="text-center pt-3">Insectphotography</h2>
             <p className="mb-0 mt-4">Welcome to my insectphotography page.</p>
             <p className="mb-0">I use Realme 3 pro mobile to take insect photos.Its 10x zoom makes the insects big!</p>
             <p className="mb-0">Here are some of the photos</p>
