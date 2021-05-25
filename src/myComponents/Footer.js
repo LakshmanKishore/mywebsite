3ed4e089413b5f6f6abdbecee7f877d50ae7d382
LakshmanKishore <lakshmankishore17.com>
2021-05-25 16:55:40 +0530

    Remove comment

--- src/myComponents/Footer.js
+++ src/myComponents/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import "../App.css";
-import { Icon, InlineIcon } from '@iconify/react';
+import { Icon } from '@iconify/react';
 import sololearnIcon from '@iconify-icons/simple-icons/sololearn';
 import codewarsIcon from '@iconify-icons/simple-icons/codewars';
 
@@ -8,11 +8,10 @@ export const Footer = () => {
     return (
         <div align="center" className="pb-4">
             <footer>
-                {/* mail github sololearn codewars */}
                 <p>You can find me here</p>
                 <div>
-                    <a href="https://github.com/LakshmanKishore" className="fa fa-github"></a>
-                    <a href="https://lakshmankishore17@gmail.com" className="fa fa-envelope"></a>
+                    <a href="https://github.com/LakshmanKishore" className="fa fa-github" />
+                    <a href="https://lakshmankishore17@gmail.com" className="fa fa-envelope" />
                     <a href="https://www.sololearn.com/profile/15282029"><Icon className="icons" icon={sololearnIcon} /></a>
                     <a href="https://www.codewars.com/users/Lakshman321"><Icon className="icons" icon={codewarsIcon} /></a>
                 </div>
