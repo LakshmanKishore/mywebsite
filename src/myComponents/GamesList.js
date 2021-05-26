4342a6f1a5215231189b00d68cb40d05bda69138
LakshmanKishore <lakshmankishore17.com>
2021-05-26 14:41:53 +0530

    Gamelist:Add new game instructions

--- src/myComponents/GamesList.js
+++ src/myComponents/GamesList.js
@@ -64,20 +64,11 @@ export const GamesList = () => {
     )
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> Footer
 // Instructions to add new games
 
 // * Make the folder name in small_caps
 // * Make the files name in small_caps
 // * Put the image in that game folder (make size 286x286)
 // * Change the css and js files link and script in html
-<<<<<<< HEAD
-// * Add the load.js file script in html
-// * Open GamesList.js and add the title and Description inside the array
-=======
 // * Add the load.js file script in particular html file
 // * Open GamesList.js and add the title and Description inside the array 
->>>>>>> Footer
