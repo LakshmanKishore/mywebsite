103a7ab1356eb657adf7d4c725366856afd1a1fc
LakshmanKishore <lakshmankishore17@gmail.com>
2024-04-07 17:21:01 +0530

    refactor: Update links section

--- src/myComponents/SocialLinks.js
+++ src/myComponents/SocialLinks.js
@@ -7,11 +7,12 @@ export const SocialLinks = ({ ulClassNames, liClassNames }) => {
         <ul className={ulClassNames}>
             <SocialLinkItem liClassNames={liClassNames} name="GitHub" href="https://github.com/LakshmanKishore" icon="eva:github-outline" />
             <SocialLinkItem liClassNames={liClassNames} name="Mail" href="mailto:lakshmankishore17@gmail.com" icon="feather:mail" />
-            <SocialLinkItem liClassNames={liClassNames} name="Codepen" href="https://codepen.io/Lakshman17/" icon="simple-icons:codepen" />
+            {/* <SocialLinkItem liClassNames={liClassNames} name="Codepen" href="https://codepen.io/Lakshman17/" icon="simple-icons:codepen" /> */}
             <SocialLinkItem liClassNames={liClassNames} name="Sololearn" href="https://www.sololearn.com/profile/15282029" icon="simple-icons:sololearn" />
-            <SocialLinkItem liClassNames={liClassNames} name="Codewars" href="https://www.codewars.com/users/Lakshman321" icon="cib:codewars" />
+            {/* <SocialLinkItem liClassNames={liClassNames} name="Codewars" href="https://www.codewars.com/users/Lakshman321" icon="cib:codewars" /> */}
             <SocialLinkItem liClassNames={liClassNames} name="LinkedIn" href="https://www.linkedin.com/in/lakshman-kishore-s-v-43a980224" icon="eva:linkedin-outline" />
-            <SocialLinkItem liClassNames={liClassNames} name="Twitter/X" href="https://twitter.com/Lakshman_2001" icon="pajamas:twitter" />
+            <SocialLinkItem liClassNames={liClassNames} name="Twitter/X" href="https://twitter.com/Lakshman007bff" icon="pajamas:twitter" />
+            <SocialLinkItem liClassNames={liClassNames} name="Hashnode" href="https://lakshman007bff.hashnode.dev" icon="simple-icons:hashnode" />
         </ul>
     )
 }
